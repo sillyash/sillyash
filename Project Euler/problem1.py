0423387c5bfee970f181fedb3b7d821a652447c5
Ashley <59657132+sillyash@users.noreply.github.com>
2024-08-17 17:09:23 +0200

    Delete Project Euler directory

--- Project Euler/problem1.py
+++
@@ -1,9 +0,0 @@
-def multiples(n1:int, n2:int, n:int)->list:
-    li = [n1]
-    for i in range(n1+1, n):
-        if (i % n1 == 0) or (i % n2 == 0):
-            li.append(i)
-    return li
-
-
-print(sum(multiples(3,5,1000)))
